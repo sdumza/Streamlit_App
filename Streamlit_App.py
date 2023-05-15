65d610b14e3fa5c89a4bf15c3247bc87aab852e6
Dumisani <56888820+sdumza@users.noreply.github.com>
2023-05-15 23:47:22 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -16,5 +16,5 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 st.dataframe(my_fruit_list)
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-st.text('Your selected friuts') 
+st.header ('Your selected fruits') 
 st.dataframe(fruits_to_show)
