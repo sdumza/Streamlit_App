406e2a84f4bc14580d8c7e06d0b2070d3e4c98f4
Dumisani <56888820+sdumza@users.noreply.github.com>
2023-05-15 12:25:16 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,6 +1,6 @@
 #created main python file
 import streamlit as st
-st.title('My Parents  New Healthy Diner')
+st.title('My Mom"'"s  New Healthy Diner')
 st.header('Breakfast Menu')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
 st.text('🥗 Kale, Spinach & Rocket Smoothie')
