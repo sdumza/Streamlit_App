078ef09f3c614eba8b3cbca80fc56d5c4b1331b0
Dumisani <56888820+sdumza@users.noreply.github.com>
2023-05-15 22:53:50 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -9,6 +9,8 @@ st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text(' 🐔Hard-Boiled Free-Range Egg')
 st.text ('🥑🍞 Advocado toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-st.dataframe(my_fruit_list)
-# Let's put a pick list here so they can pick the fruit they want to include 
+# Let's put a pick list here so they can pick the fruit they want to include
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.dataframe(my_fruit_list)
+ 
+
